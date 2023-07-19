aff9d539a33e21b49eb2868fce572970b34896aa
Darllinson Azevedo <azevedo.darllinson@gmail.com>
2023-07-19 15:52:02 -0300

    feat(home): initialize GitHub OAuth setup and create components

---
+++ src/components/Copyright.tsx
@@ -0,0 +1,15 @@
+export function Copyright() {
+  return (
+    <div className="text-sm leading-relaxed text-gray-200">
+      Feito com 💜 por{' '}
+      <a
+        href="https://github.com/darllinsonazvd"
+        target="_blank"
+        rel="noreferrer"
+        className="underline transition-colors duration-300 hover:text-gray-100"
+      >
+        Darllinson Azevedo
+      </a>
+    </div>
+  )
+}
