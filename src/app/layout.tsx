bf30cf2c30c9caee259f5bf4633aee8c2dbc0c0f
Darllinson Azevedo <azevedo.darllinson@gmail.com>
2023-07-18 21:11:26 -0300

    feat(home): implementing initial homepage layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,32 @@
 import './globals.css'
 import { ReactNode } from 'react'
-import { Inter } from 'next/font/google'
+import {
+  Roboto_Flex as Roboto,
+  Bai_Jamjuree as BaiJamjuree,
+} from 'next/font/google'
+import { Metadata } from 'next'
 
-const inter = Inter({ subsets: ['latin'] })
+const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' })
+const baiJamjuree = BaiJamjuree({
+  subsets: ['latin'],
+  weight: '600',
+  variable: '--font-bai-jamjuree',
+})
+
+export const metadata: Metadata = {
+  title: 'NLW Spacetime',
+  description:
+    'Uma cápsula do tempo construída com React, Next.js, TailwindCSS e Typescript',
+}
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={`${roboto.variable} ${baiJamjuree.variable} bg-gray-900 font-sans text-gray-100`}
+      >
+        {children}
+      </body>
     </html>
   )
 }
