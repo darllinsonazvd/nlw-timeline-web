da02576458db8ea00b9bc9fa0424faf06abcf696
Darllinson Azevedo <azevedo.darllinson@gmail.com>
2023-12-06 19:52:16 -0300

    docs(hero): replace group members list

--- src/components/Copyright.tsx
+++ src/components/Copyright.tsx
@@ -1,15 +1,7 @@
 export function Copyright() {
   return (
     <div className="text-sm leading-relaxed text-gray-200">
-      Feito com 💜 por{' '}
-      <a
-        href="https://github.com/darllinsonazvd"
-        target="_blank"
-        rel="noreferrer"
-        className="underline transition-colors duration-300 hover:text-gray-100"
-      >
-        Darllinson Azevedo
-      </a>
+      Feito com 💜 por Darllinson Azevedo, Pedro Azevedo e Pedro Henrique.
     </div>
   )
 }
